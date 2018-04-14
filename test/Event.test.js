bc2e17de14f855016758d7221ad9f96664502a3d
Marcian Diamond <marciandmnd@yahoo.com>
2018-04-14 12:47:58 -0700

    Remove uneeded comment

--- test/Event.test.js
+++ test/Event.test.js
@@ -4,7 +4,6 @@ import { expect } from 'chai';
 describe('Event', () => {
   it('should create instance of Event', () => {
     const event = new Event();
-    // expect(event).to.be
-    expect(event).to.be.an.instanceof(Event, 'nooo why fail??');
+    expect(event).to.be.an.instanceof(Event);
   });
 })
