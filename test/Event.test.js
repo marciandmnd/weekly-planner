30dbc1bd20021aecfb7c2b19143a7e4e1d67882d
Marcian Diamond <marciandmnd@yahoo.com>
2018-04-14 12:46:53 -0700

    set up testing utilities w/ mocha, chai, babel

---
+++ test/Event.test.js
@@ -0,0 +1,10 @@
+import Event from '../src/js/Event';
+import { expect } from 'chai';
+
+describe('Event', () => {
+  it('should create instance of Event', () => {
+    const event = new Event();
+    // expect(event).to.be
+    expect(event).to.be.an.instanceof(Event, 'nooo why fail??');
+  });
+})
